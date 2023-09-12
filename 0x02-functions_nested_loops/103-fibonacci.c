3a996b3e2d38a14d6363f027212de4faae6a8a6f
ship0000n <shipon0920063869@gmail.com>
2023-09-12 15:50:49 -0700

    a

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,17 +9,26 @@
 
 int main(void)
 {
+
 int i = 0;
+
 long j = 1, k = 2, sum = k;
 
 while (k + j < 4000000)
 {
+
 k += j;
+
 if (k % 2 == 0)
+
 sum += k;
+
 j = k - j;
+
 ++i;
 }
+
 printf("%ld", sum);
+
 return (0);
 }
