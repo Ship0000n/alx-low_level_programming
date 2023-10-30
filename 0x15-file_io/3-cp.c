de44c8f47edfc2d0c2ddd3b71cc02c5d86046eed
ship0000n <shipon0920063869@gmail.com>
2023-10-30 14:59:35 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -51,12 +51,13 @@ exit(100);
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
  * If file_from does not exist or cannot be read - exit code 98.
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
+ *
+ * Return: 0 on success.
 */
 
 int main(int argc, char *argv[])
