1bc03f833bd18d8470625c92401366d18492eea2
ship0000n <shipon0920063869@gmail.com>
2023-11-03 13:28:26 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,5 +14,6 @@ int main(int argc,char*argv[]){
 	while((bytes_read=read(fd_from,buffer,BUF_SIZE))>0){bytes_written=write(fd_to,buffer,bytes_read);if(bytes_written!=bytes_read){dprintf(2,"Error: Can't write to file %s\n",argv[2]);exit(99);}}
 	if(bytes_read==-1){dprintf(2,"Error: Can't read from file %s\n",argv[1]);exit(98);}
 	if(close(fd_from)==-1||close(fd_to)==-1){dprintf(2,"Error: Can't close fd\n");exit(100);}
+
 	return 0;
 }
